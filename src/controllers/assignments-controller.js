534a683b1b229dd8b4e744f20301e22a4f15026e
anvitrola <vitoriavianathiss@outlook.com>
2021-03-31 22:55:05 -0300

    Activating POST path

--- src/controllers/assignments-controller.js
+++ src/controllers/assignments-controller.js
@@ -2,6 +2,9 @@ function assignments (app){
     app.get('/assignments', (_, res) => {
         res.send(`Path activated with GET method and assignments resource; assignments values must be returned.`)
     });
+    app.post('/assignments', (_, res) => {
+        res.send(`Assignments POST path activated: assignment added to database `)
+    });
 }
 
 module.exports = assignments;
--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,6 +2,9 @@ function user (app){
     app.get('/user', (_, res) => {
         res.send(`Path activated with GET method and user resource; user values must be returned.`)
     });
+    app.post('/user', (_, res) => {
+        res.send(`User POST path activated: user added to database `)
+    });
 }
 
 module.exports = user;
