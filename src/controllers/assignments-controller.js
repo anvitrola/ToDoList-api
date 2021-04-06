6250d3bfc5be54bb1ec36230e4f05fdd05ffc4cd
anvitrola <vitoriavianathiss@outlook.com>
2021-04-06 00:11:35 -0300

    Structuring POST and GET path. Sending data to fake database just to test API results.

--- src/controllers/assignments-controller.js
+++ src/controllers/assignments-controller.js
@@ -1,13 +1,24 @@
-function assignments (app){
-    app.get('/assignments', (_, res) => {
-        res.send(`Path activated with GET method and assignments resource; assignments values must be returned.`)
-    });
-    app.post('/assignments', (_, res) => {
-        res.send(`Assignments POST path activated: assignment added to database `)
-    });
-}
+const Assignment = require('../models/assignments-model'); //importing assignment model
+const assignmentsList = require('../config/bd.js').assignments; //importing assignments table on "database"
 
-module.exports = assignments;
+module.exports = app => {
+    app.get('/assignments', (_, res) => {
+        res.send(assignmentsList); //sending list with all assignments
+    });
+    
+    app.post('/assignments', (req, res) => {
+        let body = req.body; 
+        const assignment = new Assignment(body.title, body.limitDate, body.description); //creating new assignment
+
+        //if there is not empty spaces, then push new assignment to it's table on database
+        if (body.title && body.limitDate && body.description){
+            assignmentsList.push(assignment.assignment());
+            res.send({status: 'Assignment added succesfully!'}); //"200" msg
+        } else {
+            res.send("Request error!");
+        }
+    });
+};
 
 
 
--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,12 +1,24 @@
-function user (app){
+const User = require('../models/user-model'); //importing user model
+const usersList = require('../config/bd.js').users; //importing users table on "database"
+
+module.exports = (app) => {
     app.get('/user', (_, res) => {
-        res.send(`Path activated with GET method and user resource; user values must be returned.`)
+        res.send(usersList); //sending list with all users 
     });
-    app.post('/user', (_, res) => {
-        res.send(`User POST path activated: user added to database `)
+
+    app.post('/user', (req, res) => {
+        let body = req.body;
+        const user = new User(body.name, body.username, body.email, body.password); //creating new user
+
+        //if there is not empty spaces, then push new user to it's table on database
+        if (body.name && body.username && body.email && body.password){
+            usersList.push(user.profile());
+            res.send({status: 'User created succesfully!'}); //"200" msg
+        } else{
+            res.send("Request error!");
+        }
     });
-}
-
-module.exports = user;
+};
 
 
+//const{name, username, email, password} = req.body;
