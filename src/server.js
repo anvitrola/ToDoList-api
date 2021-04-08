787284329354194335b060bd6db562813469ff98
anvitrola <vitoriavianathiss@outlook.com>
2021-04-08 18:57:18 -0300

    Revert "Stablishing database connection and creating its tables"
    This reverts commit e0106fa73f8f6ab17e4d4eeada367d5339aad9a1.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,12 @@
 const chalk = require("chalk"); //painting terminal to becomes easier to find errors
 const customExpress = require("./config/customExpress"); //requiring server configurations (customExpress)
-const connection = require("./infra/bd-connection");
-const Tables = require("./infra/tables");
 
-connection.connect((error) => {
-    if (error){
-        console.log(error)
+const app = customExpress();
 
-    } else{
-        console.log(chalk.green("successfully connected!"));
-        Tables.init(connection);
-        const app = customExpress();
-        
-        app.use((req, _, next) => {
-            console.log(chalk.yellow(`[INFO]: Request ${req.method} type.`));
-            next();
-        }); //announcing the type of request
-        
-        //openning the server, exposing port etc.
-        app.listen("2000", () => console.log(chalk.green("[SUCESSO]: Example app listening at http://localhost:2000")));
-    }
-});
+app.use((req, _, next) => {
+    console.log(chalk.yellow(`[INFO]: Request ${req.method} type.`));
+    next();
+}); //announcing the type of request
+
+//openning the server, exposing port etc.
+app.listen("2000", () => console.log(chalk.green("[SUCESSO]: Example app listening at http://localhost:2000")));
