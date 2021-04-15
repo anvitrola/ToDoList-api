1fd9b1fe67d7443ddec3766f070f1087bb0ebaaf
anvitrola <vitoriavianathiss@outlook.com>
2021-04-14 22:38:13 -0300

    Starting unit tests and adding cors to protect api

---
+++ tests/assignments-controller.test.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk')
+const request = require('supertest');
+
+const express = require('express');
+const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../src/infra/database.db');
+
+const controller = require('../src/controllers/assignments-controller')(app, db);
+
+request(app)
+    .get('/assignments')
+    .expect(200)
+    .end((err, res) => {
+        if(err) throw(err)
+        else console.log(chalk.green("Your test PASSED!!!"))
+    });
+
